3696f610ef5e366d50d72023382ba1eab8b41d4d
Shiv Patel <smitpkako@gmail.com>
2024-09-18 15:26:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 blocked = "bot was blocked by the user"#message that would be displayed if user has blocked you
-start = "if you are limited and want to message @mtbdevs then type your message here, @mtbdevs can see the message directly through the bot"#start command
+start = "if you are limited ✨ and want to message @mtbdevs then type your message here,😁 @mtbdevs can see the message directly through the bot"#start command
 ban = "You are blocked by @mtbdevs i dont know why but 🤣"#ban message
 unban = "you were unbanned by the admin."#unban message
-text_message = "Okay thank you, wait i will response you when i am online"#answering_text to user
+text_message = "Admin will reply you soon"#answering_text to user
 banned = "You are blocked by @mtbdevs i dont know why but 🤣"#if admin has blocked a user
 main_id = 7358307430 #id without quotes #can be group id
-TOKEN = "7100398900:AAElgN374BNYoza7I7kBXYQ3IKj4MmPZ1sg"#bot token
+TOKEN = "6961261674:AAEf9NbZq5sMjnjKnGStHFYASeYiNdUtVMo"#bot token
